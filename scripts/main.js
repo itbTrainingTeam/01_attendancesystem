83aeaa676103489a3626781ca4965a59cdc10678
Shuto Keisuke <shutobsac1997@gmail.com>
2021-04-16 10:29:40 +0900

    Create common-header

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,9 @@ class Main {
     }
 
     _init() {
+        new LoadHtml('#admin-header', 'common/admin-header.html')
+        new LoadHtml('#user-header', 'common/user-header.html')
         new ToggleClassOnClick('open', '.header-mobile-menu', '.header-menu');
+        
     }
 }
